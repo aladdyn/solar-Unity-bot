97970d9efc3be1712ee4d4e2278868f31bd553cc
* <allanvieira78596@gmail.com>
2021-03-25 01:04:52 -0300

    First commit

---
+++ solar-Unity-bot-main/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/554196979697*
+║
+╚═〘  CORVO  〙
+`
+}
+exports.iklan = iklan
---
+++ solar-Unity-bot-main/pack.js
@@ -0,0 +1,16 @@
+const pack = (prefix) => {
+    return `*PACKS:*
+    
+CORVO GOSTOSAO 
+
+Pack1: https://photos.app.goo.gl/phyRpNFLcAtsknfJ7 
+
+Kitty kum: https://photos.app.goo.gl/gNxLbJHGVzeWY9iS9 
+
+Misaki Your Waifu: https://photos.app.goo.gl/M9nyHdonAJu5GRjZ9 
+
+pack: https://photos.app.goo.gl/SrogbCbnanL2PJQn7 
+
+`
+}
+exports.pack = pack
